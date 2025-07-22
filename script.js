97346466709190dd5c8b08a293f463069c20763e
Edidi Sai Anant <89198001+ESAnant@users.noreply.github.com>
2025-07-22 23:22:04 +0800

    Phase 2 revision 2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// --- PHASE 1 & 2: NO JAVASCRIPT ACTION REQUIRED ---
+// --- PHASE 2.1: NO JAVASCRIPT ACTION REQUIRED ---
 
 document.addEventListener('DOMContentLoaded', () => {
     // Phase 3: Core interactivity and animation logic will be added here.
-    console.log("Portfolio content loaded. Ready for Phase 3 interactivity.");
+    console.log("Portfolio content and navigation loaded. Ready for Phase 3 interactivity.");
 });
